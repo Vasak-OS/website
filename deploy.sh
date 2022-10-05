e7853eeaab22691511bd55c3252c8c025f31d31b
Joaquin (Pato) Decima <jdecima@teco.com.ar>
2022-10-05 10:16:03 -0300

    Add deploy

---
+++ deploy.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# abort on errors
+set -e
+# build
+hugo # if using a theme, replace with `hugo -t <YOURTHEME>`
+
+# navigate into the build output directory
+cd public
+
+git init
+git add -A
+git commit -m 'deploy'
+
+# if you are deploying to https://<USERNAME>.github.io/<REPO>
+git push -f git@github.com:Vasak-OS/website.git main:gh-pages
+cd -
