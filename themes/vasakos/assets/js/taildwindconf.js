4994542f90f8a04645209f58663fbaa479261b02
Joaquin (Pato) Decima <jdecima@bancodelsol.com>
2024-01-11 16:28:13 -0300

    Add tailwind configuration file

---
+++ themes/vasakos/assets/js/taildwindconf.js
@@ -0,0 +1,14 @@
+tailwind.config = {
+  darkMode: 'class',
+  theme: {
+    fontFamily: {
+      sans: ['Poppins', 'sans-serif'],
+    },
+    extend: {
+      colors: {
+        vsk1: '#0BC98C',
+        vsk2: '#007CC3',
+      }
+    }
+  }
+}
