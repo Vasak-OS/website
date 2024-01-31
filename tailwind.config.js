a5f41630ecd31563fa77948f66dff9e8a0da1726
Joaquin (Pato) Decima <jdecima@bancodelsol.com>
2024-01-31 19:54:52 -0300

    Add @tailwindcss/forms dependency and configure tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   darkMode: 'class',
+  plugins: [
+    require('@tailwindcss/forms'),
+  ],
   content: [
     './themes/vasakos/**/*.html',
     './themes/vasakos/**/*.js',
