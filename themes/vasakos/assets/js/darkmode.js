4e025a451d0f68dd06f1bd22202c71ee4b248e7a
Joaquin (Pato) Decima <jdecima@pickit.net>
2026-03-12 12:34:12 -0300

    feat: Enhance dark mode support and update color classes for improved design consistency across templates

--- themes/vasakos/assets/js/darkmode.js
+++ themes/vasakos/assets/js/darkmode.js
@@ -2,6 +2,7 @@ const darkmode = getDarkmode();
 const toggleDarkmodeButton = document.getElementById('toggletheme');
 
 if (darkmode === 'true') {
+  document.documentElement.classList.add('dark');
   document.body.classList.add('dark');
   toggleDarkmodeButton.innerHTML = '<em class="fas fa-sun"></em>';
 }
@@ -15,11 +16,13 @@ function setDarkmode(value) {
 }
 
 function toggleDarkmode() {
-  if (document.body.classList.contains('dark')) {
+  if (document.documentElement.classList.contains('dark')) {
+    document.documentElement.classList.remove('dark');
     document.body.classList.remove('dark');
     setDarkmode('false');
     toggleDarkmodeButton.innerHTML = '<em class="fas fa-moon"></em>';
   } else {
+    document.documentElement.classList.add('dark');
     document.body.classList.add('dark');
     setDarkmode('true');
     toggleDarkmodeButton.innerHTML = '<em class="fas fa-sun"></em>';
